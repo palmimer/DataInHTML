7d5b310c3b22b18039f0d25e3f31126504ed07ed
Palmai Mercedesz <mercedesz.palmai@dextraline.hu>
2020-11-12 17:48:12 +0100

    data is read from file and written to console

---
+++ DataInHTML/FileReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataInHTML
+{
+    class FileReader
+    {
+        public String[] readCsvFileLines(string filePath)
+        {
+            string[] lines = new string[] {};
+            lines = File.ReadAllLines(filePath);
+            return lines;
+        }
+
+        public string[] getDataFromLine(string line)
+        {
+            string[] data = line.Split(",");
+            for(int i = 0; i < data.Length; i++)
+            {
+                data[i] = data[i].Trim();
+            }
+            return data;
+        }
+
+    }
+}
---
+++ DataInHTML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataInHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileReader fileReader = new FileReader();
+            string[] allLines = fileReader.readCsvFileLines("C:/Users/dextra/Documents/DextraLineProjects/Adatmegjelenítés/CSV fájlok/cimek_teszt.csv");
+            string[] separatedLine = fileReader.getDataFromLine(allLines[0]);
+            Console.WriteLine("Hello Merci!");
+            Console.WriteLine(allLines[0]);
+            int i = 0;
+            while (i < separatedLine.Length)
+            {
+                Console.WriteLine((i + 1) + ". " + separatedLine[i]);
+                i++;
+            }
+            Console.WriteLine("Nyomj meg egy billentyűt a kilépéshez!");
+            Console.ReadKey();
+        }
+    }
+}
